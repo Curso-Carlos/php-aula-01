9837e17eb29051b551a8026c15f19300b840b5f1
Jefferson Frade <jefferrson.frade@gmail.com>
2020-08-04 19:52:34 -0300

    Variaveis

---
+++ 01.php
@@ -0,0 +1,16 @@
+<?php
+    $nome = 'Jefferson';
+    
+    $num1 = 15;
+    $num2 = 10;
+
+    echo $num1 + $num2;
+    echo '<br/>';
+    echo $num1 - $num2;
+    echo '<br/>';
+    echo $num1 * $num2;
+    echo '<br/>';
+    echo $num1 / $num2;
+    echo '<br/>';
+    echo $num1 % $num2;
+?>
